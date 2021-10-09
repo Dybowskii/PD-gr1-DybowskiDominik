f93711b4727f473e0633acc4e31081e70be3bc70
Dybowski <73537083+Dybowskii@users.noreply.github.com>
2021-10-09 13:54:29 +0200

    Add files via upload

---
+++ labo1/zad11/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Nic dwa razy się nie zdarza");
+        System.out.println("i nie zdarzy. Z tej przyczy");
+        System.out.println("zrobilismu sie bez wprawy");
+        System.out.println("i pomrzemy bez rutyny");
+
+    }
+}
---
+++ labo1/zad4/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double saldo=1000;
+        System.out.println(saldo);
+        saldo=saldo*1.06;
+        System.out.println(saldo);
+        saldo=saldo*1.06;
+        System.out.println(saldo);
+
+    }
+}
