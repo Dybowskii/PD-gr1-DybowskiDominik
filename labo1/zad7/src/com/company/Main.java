3df443a6ad2a9493ceeb9aaf396299432ffc1155
Dybowski <73537083+Dybowskii@users.noreply.github.com>
2021-10-13 00:44:51 +0200

    Add files via upload

---
+++ labo1/zad7/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("XXXX    XXX   XX     XX  X  XX     X  X  X  X  ");
+        System.out.println("X   X  X   X  X X   X X  X  X X    X  X  X X   ");
+        System.out.println("X   X  X   X  X  X X  X  X  X  X   X  X  XX    ");
+        System.out.println("X   X  X   X  X   X   X  X  X   X  X  X  X X   ");
+        System.out.println("X   X  X   X  X       X  X  X    X X  X  X  X  ");
+        System.out.println("XXXX    XXX   X       X  X  X     XX  X  X   X ");
+    }
+}
